f85292cc18ee81a883ab8a040d2380822e62c489
Oleh Berezin <ziccer1980@gmail.com>
2023-03-19 21:13:54 +0200

    Observable Event Input, Click

--- src/rxjs-lessons.ts
+++ src/rxjs-lessons.ts
@@ -1,22 +1,33 @@
-import {debounce, debounceTime, distinctUntilChanged, fromEvent, map, Observable, observable} from "rxjs";
+import {
+  debounce, debounceTime,
+  distinctUntilChanged, fromEvent,
+  map, Observable, observable, takeUntil
+} from "rxjs";
 
-/*const search$: Observable<Event> = fromEvent<Event>(
-  document.getElementById('search'),
+
+const search$: Observable<Event> = fromEvent<Event>(
+  document.getElementById('search') as HTMLInputElement,
   'input'
-);*/
+);
+const stop$: Observable<Event> = fromEvent<Event>(
+  document.getElementById('stop') as HTMLInputElement,
+  'click'
+);
 
-const search$ = new Observable<Event>(observer => {
+/*const search$ = new Observable<Event>(observer => {
 
   const search = document.getElementById('search');
 
   if (!search){
     observer.error("Element not found!")
+    return;
   }
   search!.addEventListener("input", event => {
+    console.log(123)
     observer.next(event);
-
+    observer.complete();
   })
-});
+});*/
 // VARIANT 1
 search$.pipe(
   map(event => {
@@ -24,7 +35,8 @@ search$.pipe(
   }),
   debounceTime(500),
   map(value => value.length > 3 ? value : '' ),//ждет 500 мс, и только потом считывает значение с инпута
-  distinctUntilChanged()) // проверяет изменилось ли значение или нет, если нет значение вперед не проходит
+  distinctUntilChanged(), // проверяет изменилось ли значение или нет, если нет значение вперед не проходит
+  takeUntil(stop$))
   .subscribe((value)=> {
   console.log(value);
 })
